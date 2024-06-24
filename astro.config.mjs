fe2ac3bcb960f9439861e495f2431caba81e5d71
Elias Mamalias <mamalias23@gmail.com>
2024-06-25 07:19:14 +0800

    fix MissingSharp issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(), // https://docs.astro.build/en/reference/errors/missing-sharp/
+  },
   site: 'https://emamalias.github.io',
   base: 'astro-landing-starter',
   integrations: [
